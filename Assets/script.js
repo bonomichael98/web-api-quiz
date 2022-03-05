d78a77f4bcaacf2b183a2e8f49615faee9e2c596
michael <shinlin09@gmail.com>
2022-03-05 15:40:35 -0500

    test

--- Assets/script.js
+++ Assets/script.js
@@ -1,1 +1,14 @@
+let startButton = document.getElementById('start');
+let scoresButton = document.getElementById('display-scores');
 
+// startButton = document.getElementById('start').addEventListener('click', function() {
+//     console.log('start clicked');
+// });
+
+// scoresButton.addEventListener('click', test);
+
+// let test = console.log('button has been clicked');
+
+document.getElementById('start').addEventListener('click', function() {
+    console.log('start clicked');
+ });
