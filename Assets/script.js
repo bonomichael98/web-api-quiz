9f8149eb169141e19b7fdb029183c66223821826
michael <shinlin09@gmail.com>
2022-03-12 17:18:17 -0500

    buttons

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,20 @@
 let startButton = document.getElementById('start');
 let scoresButton = document.getElementById('display-scores');
+let section1 = document.getElementById('section-1');
+let questionsSection = document.getElementById('cycled-questions');
+let answer1 = document.getElementById('answer1');
+let answer2 = document.getElementById('answer2');
+let answer3 = document.getElementById('answer3');
+let answer4 = document.getElementById('answer4');
+let answersElement = document.getElementById('answers');
+
+
 let timeRemaining = 60;
 let score = 0;
-let questionNumber = 0;
+let questionNumber = [];
 let countdownTimerDisplay = document.getElementById('time-remaining');
 let questions = [{
-        question : "Which country do cities of Perth, Adelade & Brisbane belong to? ",
+        title : "Which country do cities of Perth, Adelade & Brisbane belong to? ",
         answers: [
             { text:"Australia", answer: true },
             { text:"Russia", answer: false },
@@ -13,7 +22,7 @@ let questions = [{
             { text:"Uruguay", answer: false }
         ]},
         {
-        question : "What is \"cynophobia\"?",
+            title : "What is \"cynophobia\"?",
         answers: [
             { text:"Fear of snakes", answer: false },
             { text:"Fear of dogs", answer: true },
@@ -21,7 +30,7 @@ let questions = [{
             { text:"Fear of sleeping", answer: false }
         ]},
         {
-        question : "Who was the first woman pilot to fly solo across the Atlantic?",
+            title : "Who was the first woman pilot to fly solo across the Atlantic?",
         answers: [
             { text:"Amelia Earhart", answer: true },
             { text:"Bessie Coleman", answer: false },
@@ -30,7 +39,7 @@ let questions = [{
             
         ]},
         {
-        question : "What is the name of the biggest technology company in South Korea?",
+            title : "What is the name of the biggest technology company in South Korea?",
         answers: [
             { text:"POSCO", answer: false },
             { text:"KIA", answer: false },
@@ -38,7 +47,7 @@ let questions = [{
             { text:"Samsung", answer: true }
         ]},
         {
-        question : "Worship of Krishna is observed by which Religious Faith?",
+            title : "Worship of Krishna is observed by which Religious Faith?",
         answers: [
             { text:"Paganism", answer: false },
             { text:"Islam", answer: false },
@@ -47,56 +56,6 @@ let questions = [{
             ]
 }];
 
-// let timer = function runTimer(timeRemaining){
-//     setTimeout(function() {
-//         console.log('hello');
-//         timeRemaining++;
-//         if (timeRemaining <= 0) {
-//             runTimer();
-//             console.log('hello')
-//         } else {
-//             endQuiz();
-//         }
-//     }, 1000)
-//}
-//
-
-
-//  let timer = function runTimer() {
-//  for (let i = 60; i <= 0; i--) {
-//      console.log('hello')
-//   }
-//  for (let i = 60; i <= 0; --i) {
-//      console.log('hello');
-//   }
-
-// let endQuiz = function() {
-//     for(let i = 60; i <= 0; i-0) {
-//         console.log('1');
-//     }
-// }
-
-// let timer = setInterval(function() {
-    // timeRemaining--;
-    // countdownTimerDisplay.textContent = 'You have ' + timeRemaining + ' seconds remaining.';
-    // if(timeRemaining <= 0) {
-        // console.log('end the timer');
-        // countdownTimerDisplay.textContent = 'You have 0 seconds remaining.';
-    // }else if(questionNumer = 4 ) {
-        // window.localStorage.setItem('playerScore', timeRemaining);
-        //clearInterval(timer);
-    // }
-// },1000)
-
-
-// startButton = document.getElementById('start').addEventListener('click', function() {
-//     console.log('start clicked');
-// });
-
-// scoresButton.addEventListener('click', test);
-
-// let test = console.log('button has been clicked');
-//stop page from refreshing on button clicks
 let form = document.getElementById('main-form');
 function submitForm(event) {
     event.preventDefault();
@@ -107,18 +66,11 @@ form.addEventListener('submit', submitForm);
 
 //function to run when you click the start button
 document.getElementById('start').addEventListener('click', function() {
-    console.log('start clicked');
     hideElement();
+    addButtons();
+    cycleText();
 
- });
 
- //function to run when you click the display scores button
- document.getElementById('display-scores').addEventListener('click', function() {
-    console.log('scores clicked');
-    hideElement();
- });
-
-// let start = startGame() {
     let timer = setInterval(function() {
         timeRemaining--;
         countdownTimerDisplay.textContent = 'You have ' + timeRemaining + ' seconds remaining.';
@@ -129,41 +81,56 @@ document.getElementById('start').addEventListener('click', function() {
             window.localStorage.setItem('playerScore', timeRemaining);
             //clearInterval(timer);
         }
-    },1000)
+    },1000);
+
+
+});
+
+ //function to run when you click the display scores button
+ document.getElementById('display-scores').addEventListener('click', function() {
+    console.log('scores clicked');
+    hideElement();
+    //displayScores();
+ });
+
+
 //     //create function to display buttons
+let checkAnswer = function() {
+    if(questions.answers === true) {
 
-// };
-
-let hideElement = function hide() {
-    // let element1 = document.getElementById("start");
-    // let element2 = document.getElementById("displayScores");
-    // startButton.classList.add("hide");
-    // startButton.classList.remove("buttons");
-
-    // scoresButton.classList.add("hide");
-    // scoresButton.classList.remove("buttons");
-    startButton.remove();
-    scoresButton.remove();
+    }
 }
 
-let createButton = document.createElement("button");
-//let text = document.createTextNode();
-//let text = document.createTextNode(questions[questionNumber])
+let hideElement = function(){
+    startButton.classList.add('hidden');
+    scoresButton.classList.add('hidden');
+};
 
+let addButtons = function(){
+    section1.classList.remove('hidden');
+};
 
-// for (let i = timeRemaining; i <= 0; i--) {
-//     //create option element
-//     let createButtons = document.createElement("button");
-//     statusOptionEl.textContent = statusChoices[i];
-//     statusOptionEl.setAttribute("value", statusChoices[i]);
+let cycleText = function(){
+    for (let i = 0; i < questions.length; i++) {
+        questionNumber = questions[i];
+        console.log(questionNumber);
+        //questions.title.forEach();
+        questionNumber.answers.forEach(function(answers, i) {
+            let answerButton = document.createElement('button');
+            answerButton.setAttribute('class', 'buttons answer');
+            answerButton.setAttribute('value', answers);
+            answerButton.textContent = answers;
+//            answerButton.onClick = checkAnswer();
+            answersElement.appendChild(answerButton);
+        })
+    }
+};
 
-//     //append to select
-//     statusSelectEl.appendChild(statusOptionEl);
-// }
 
 //create for loop that counts backwards from 60, begins when start button is selected
 
 //create function that appends buttons with questions[] array at current position when the variable with the correct attribute is selected
+//let startQuestions =
 
 //add class 'buttons' to all newly created buttons
 
